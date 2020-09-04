c1683fc6f69b93a1495255ba77064e2ebaf6baa1
Matusalem Garcia <matusalem@workinformatica.com.br>
2020-09-04 16:23:10 -0300

    update server

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -18,37 +18,31 @@ exports.handler = async function(e, context, callback) {
   const transporter = nodemailer.createTransport(transport)
     
 
-  const payload = JSON.parse(e.body).payload;
+  const params = JSON.parse(e.body);
 
-  console.log(payload)
-
-  // const { Nome, email, telefone, message, Produtos} = payload
-
-  // console.log(Nome, email, telefone, message, Produtos)
-
-  // const nome = params.Nome
-  // const email = params.email
-  // const telefone = params.telefone
-  // const message = params.message
-  // const produtos = params.Produtos
+  const nome = params.Nome
+  const email = params.email
+  const telefone = params.telefone
+  const message = params.message
+  const produtos = params.Produtos
   
-  // const content = `Nome do Cliente: ${nome}
-  //             \nEmail: ${email}
-  //             \nTelefone: ${telefone}
-  //             \nMensagem: ${message}
-  //             \nProdutos: ${produtos} 
-  //             `
-  // const mail = {
-  //     from: nome,
-  //     to: 'italocod@hotmail.com',  // Change to email address that you want to receive messages on
-  //     subject: `Mensagem de Pedido do Cliente: ${nome}`,
-  //     text: content
-  // }
+  const content = `Nome do Cliente: ${nome}
+              \nEmail: ${email}
+              \nTelefone: ${telefone}
+              \nMensagem: ${message}
+              \nProdutos: ${produtos} 
+              `
+  const mail = {
+      from: nome,
+      to: 'italocod@hotmail.com',  // Change to email address that you want to receive messages on
+      subject: `Mensagem de Pedido do Cliente: ${nome}`,
+      text: content
+  }
 
-  // return transporter.sendMail(mail)
-  // .then(() => {
-  //   callback(null, { statusCode: 200, body: 'Success' });
-  // })
-  // .catch(e => callback(e, { statusCode: 500, body: 'Error sending email' }));
+  return transporter.sendMail(mail)
+  .then(() => {
+    callback(null, { statusCode: 200, body: 'Success' });
+  })
+  .catch(e => callback(e, { statusCode: 500, body: 'Error sending email' }));
 
 }
